5aceb5f8459014ea637a6b98e754c63995d04d53
Deuxfleures <poporring@hotmail.com>
2026-09-16 08:56:53 +0200

    Commit - BattleOn server maj 0.1.21

--- src/main/kotlin/CardCatalog.kt
+++ src/main/kotlin/CardCatalog.kt
@@ -16,6 +16,7 @@ object CardCatalog {
         Card(CardId.ARAIGNEESOLDAT, 3, 0, CardFaction.BEAST, purchaseLimit = null, false, false, true),
         Card(CardId.NOURRICEARAIGNEE, 2, 0, CardFaction.BEAST, purchaseLimit = null, false, false, true),
         Card(CardId.OEUFDARAIGNEE, 0, 0, CardFaction.BEAST, purchaseLimit = null, false, false, true),
+        Card(CardId.GOLEMDOCRE,8,0, CardFaction.MERCENARY, purchaseLimit = null, false, false, false),
 
         Card(CardId.AGILE, 4, 4,CardFaction.BEAST, purchaseLimit = null,false,false,false),
         Card(CardId.ARAIGNEEGEANTE, 3, 5,CardFaction.BEAST, purchaseLimit = null,false,false,true),
@@ -115,6 +116,7 @@ object CardCatalog {
         CardId.ARAIGNEESOLDAT,
         CardId.NOURRICEARAIGNEE,
         CardId.OEUFDARAIGNEE,
+        CardId.GOLEMDOCRE,
 
         // =====================================================
         // MERCENAIRES (UNIQUEMENT MODE SAISON)
